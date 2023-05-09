4677a268dc5fdfaa544c60b776dc9836dd1cab81
Burcu Deniz <122237317+burcudenizz@users.noreply.github.com>
2023-05-09 10:29:15 +0300

    routelar belirlendi.

--- src/App.js
+++ src/App.js
@@ -3,16 +3,26 @@ import Header from "./components/Header";
 import Login from "./components/Login";
 import Mainpage from "./components/Mainpage";
 import Signup from "./components/Signup";
-import { BrowserRouter } from "react-router-dom";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
 function App() {
   return (
-    <div className="App">
+    <BrowserRouter>
       <Header />
-      <Mainpage />
-      <Signup />
-      <Login />
-      <Profile />
-    </div>
+      <Switch>
+        <Route exact path="/">
+          <Mainpage />
+        </Route>
+        <Route exact path="/signup">
+          <Signup />
+        </Route>
+        <Route exact path="/login">
+          <Login />
+        </Route>
+        <Route exact path="/profile">
+          <Profile />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
