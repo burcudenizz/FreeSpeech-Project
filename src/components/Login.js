243fd12168741cb4921b94d00896b819ad3fe279
hosak94 <yigitcanhosak@gmail.com>
2023-05-09 10:48:00 +0300

    login page

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,5 +1,46 @@
+import React, { useState } from "react";
+
 function Login() {
-  return <div></div>;
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  function handleUsernameChange(event) {
+    setUsername(event.target.value);
+  }
+
+  function handlePasswordChange(event) {
+    setPassword(event.target.value);
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    // login işlemleri burada yapılacak
+  }
+
+  return (
+    <div>
+      <h1>FreeSpeach</h1>
+      <h3>Log In</h3>
+
+      <form onSubmit={handleSubmit}>
+        <label>
+          Email:
+          <input type="text" value={username} onChange={handleUsernameChange} />
+        </label>
+        <br />
+        <label>
+          Passowrd:
+          <input
+            type="password"
+            value={password}
+            onChange={handlePasswordChange}
+          />
+        </label>
+        <br />
+        <button type="submit">Giriş Yap</button>
+      </form>
+    </div>
+  );
 }
 
 export default Login;
