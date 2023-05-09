7ca584f860d6b4bdb2fea7890e893f5c796d5ff7
Burcu Deniz <122237317+burcudenizz@users.noreply.github.com>
2023-05-09 14:18:07 +0300

    mainpage güncellendi.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,5 +1,13 @@
 import React from "react";
 
 export default function Header() {
-  return <div>bu header</div>;
+  return (
+    <div className="header">
+      <img
+        className="logo"
+        src="https://static-00.iconduck.com/assets.00/text-to-speech-icon-512x426-lx0859lj.png"
+      />
+      <h1> FreeSpeech</h1>
+    </div>
+  );
 }
--- src/components/Mainpage.js
+++ src/components/Mainpage.js
@@ -1,5 +1,25 @@
 function Mainpage() {
-  return <div>mainpage</div>;
+  return (
+    <div className="mainpageContainer">
+      <div className="tweetContainer">
+        <div className="tweets">tweets</div>
+      </div>
+      <div className="footerback">
+        <div className="footer">
+          <div className="footerCenter">
+            <div className="footerContent">
+              <button>Sign Up</button>
+              <br />
+              <div className="login">
+                <h2>Do you already have an account? 👀</h2>
+                <button>Log In</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Mainpage;
